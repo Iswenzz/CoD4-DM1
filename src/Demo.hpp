80df9771302fd4e6f505131ce3f172eed7b7ebb6
iswenzz <suxlolz1528@gmail.com>
2020-05-27 18:27:22 +0200

    Commented Demo class

--- src/Demo.hpp
+++ src/Demo.hpp
@@ -9,14 +9,35 @@ namespace Iswenzz
 	struct ClientArchiveData;
 	class Msg;
 
+	/// <summary>
+	/// Represent a server demo file .dm_1
+	/// </summary>
 	class Demo
 	{
 		public:
+			/// <summary>
+			/// Initialize a new Demo object, demo file can be opened with the open() function.
+			/// </summary>
+			/// <returns></returns>
 			Demo();
+
+			/// <summary>
+			/// Initialize a new Demo object with the specified demo file path.
+			/// </summary>
+			/// <param name="filepath"></param>
+			/// <returns></returns>
 			Demo(std::string filepath);
 			~Demo();
 
+			/// <summary>
+			/// Open a demo file from specified path.
+			/// </summary>
+			/// <param name="filepath">File path to a demo file (.dm_1).</param>
 			void open(std::string filepath);
+
+			/// <summary>
+			/// Close the demo file and free resources.
+			/// </summary>
 			void close();
 
 		private:
@@ -27,8 +48,19 @@ namespace Iswenzz
 			std::vector<ClientSnapshotData> snapshots;
 			std::vector<ClientArchiveData> archive;
 
+			/// <summary>
+			/// Read demo file header.
+			/// </summary>
 			void readHeader();
+
+			/// <summary>
+			/// Read an archive client state.
+			/// </summary>
 			void readArchive();
+
+			/// <summary>
+			/// Read a server snapshot.
+			/// </summary>
 			void readSnapshot();
 	};
 }
