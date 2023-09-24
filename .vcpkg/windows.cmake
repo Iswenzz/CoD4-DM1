c1b520ac8127f4f57ee4cf581ea49883c15f316b
Iswenzz <alexisnardiello@gmail.com>
2023-09-25 01:49:47 +0200

    Vcpkg: Environment

--- .vcpkg/windows.cmake
+++ .vcpkg/windows.cmake
@@ -1,4 +1,3 @@
-include(.vcpkg/cmake/env.cmake)
 set(VCPKG_TARGET_TRIPLET x64-windows-static)
 
 option(BUILD_CLI "Build CLI" OFF)
@@ -11,4 +10,8 @@ if(BUILD_TESTING)
 	list(APPEND VCPKG_MANIFEST_FEATURES "tests")
 endif()
 
+if(NOT DEFINED ENV{VCPKG_ROOT})
+	message(FATAL_ERROR "ERROR: VCPKG_ROOT environment variable is not defined. Please set it to the root directory of your vcpkg installation.")
+endif()
+file(TO_CMAKE_PATH $ENV{VCPKG_ROOT} VCPKG_ROOT)
 include(${VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake)
