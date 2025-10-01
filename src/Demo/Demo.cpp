629ac0474f90c2974ee85eb07ba2e90baec6e6d8
Caball009 <82909616+Caball009@users.noreply.github.com>
2025-10-01 16:17:37 +0200

    Check for out-of-bound value.

--- src/Demo/Demo.cpp
+++ src/Demo/Demo.cpp
@@ -1153,7 +1153,12 @@ namespace CoD4::DM1
 		for (i = 0; i < inuse; ++i)
 		{
 			lc = msg.ReadBits(6);
-			assert(lc <= sizeof(NetFields::HudElemFields) / sizeof(NetFields::HudElemFields[0]));
+			if (static_cast<unsigned int>(lc) > HUD_ELEM_FIELDS_COUNT)
+			{
+				VerboseLog("Error parsing hud elements");
+				msg.Overflowed = true;
+				return;
+			}
 
 			for (y = 0; y <= lc; ++y)
 				ReadDeltaField(msg, time, &from[i], &to[i], &NetFields::HudElemFields[y], false, false);
@@ -2186,4 +2191,5 @@ namespace CoD4::DM1
 		return true;
 	}
 }
+
 
